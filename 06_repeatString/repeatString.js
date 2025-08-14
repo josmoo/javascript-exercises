e6e712d0fd08d91e134356689c5c7d8da5ee8b3b
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-14 11:16:28 -0500

    did some exercises, temporarily stuck on one :)

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(strng, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    result = ""
+    for (let index = 0; index < num; index++){
+        result += strng;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wordLength = word.length;
+    var stack = [];
+    var reversedWord = "";
+    for(index = 0; index < wordLength; index++){
+        console.log(index + word[index]);
+        stack.push(word[index]);
+    }
+    for(index = wordLength - 1; index >= 0; index--){
+        reversedWord += stack.pop[index];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
