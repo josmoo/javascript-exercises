d1f03e1628ddff48baeb2dcb3700a19a6e2a0078
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-19 11:27:12 -0500

    why the FIAOSJDFUIOAEWF did i not use filter AHHH

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    targets.map(target => removeElement(arr, target, 0));
+    return arr;
 };
+
+const removeElement = function(arr, target, i){
+    let index = arr.indexOf(target, i);
+    if(index !== -1){
+        arr.splice(index, 1)
+        removeElement(arr, target, index)
+    };
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
