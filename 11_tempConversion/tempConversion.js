149c609f7b5caa585db3c153c1d6945655a024de
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-19 15:01:30 -0500

    finished 10 and 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemperature) {
+  return Math.round(10*((fTemperature - 32) * (5/9)))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemperature) {
+  return Math.round(10*((cTemperature * 9/5) + 32))/10;
 };
 
 // Do not edit below this line
