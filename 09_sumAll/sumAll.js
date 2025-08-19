4fa365a3c3e1c02477f15045614d4b2e8c7c7ed6
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-19 11:47:48 -0500

    worked on sumall a little bit, still needs a ton of work

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if(!begin.isInteger() || begin < 0 || !end.isInteger || end < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    for(begin; begin <= end; begin++){
+        sum+=begin;
+    }
+    return sum;
 };
 
 // Do not edit below this line
