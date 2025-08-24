0798201d9a7ede06f8f88d0bc59fd5fe962e6462
josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-23 20:33:13 -0500

    finished the exercises YAAAAAAYYYYYYYYY ♥

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+            return (calculateAge(oldest) > calculateAge(person)) ? oldest : person
+        }
+    );
 };
+
+const calculateAge = function(person) {
+    return (person.yearOfDeath == null) 
+    ? (new Date().getFullYear() - person.yearOfBirth)
+    : (person.yearOfDeath - person.yearOfBirth);
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
